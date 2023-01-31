534652fee9f45e0c9db9d1e081410beddfd7a528
robmortera93 <robmrtra@gmail.com>
2023-01-30 21:28:25 -0600

    update

--- src/store/index.jsx
+++ src/store/index.jsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import isLoadingSlice from "./slices/isLoading.Slice";
 import productsSlice from "./slices/products.Slice";
+import purchasesSlice from "./slices/purchases.slice";
 
 export default configureStore({
   reducer: {
     isLoading: isLoadingSlice,
     products: productsSlice,
+    purchases: purchasesSlice,
   },
 });
